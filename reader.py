1cfa3687648a6b2293284b8201d331b7b7d6a8be
Vitor <vitorcooutinho@gmail.com>
2025-07-04 16:05:21 -0300

    fix: fix reader and change example

--- reader.py
+++ reader.py
@@ -1,13 +1,33 @@
+# pylint: disable = "C0114, C0116, W0621, W0719"
+
+
 def reader(path):
-    words = []
-    with open(path, 'r', encoding='utf-8') as file:
-        for line in file:
-            for symbol in [',', ';', '(', ')']:
-                line = line.replace(symbol, f' {symbol} ')
-            for word in line.split():
-                words.append(word)
-    return words
-    
-words = reader('code.txt')
-for word in words:
-    print(word)
+    """
+    Função que lê o arquivo de código e destaca as
+    palavras reservadas e operadores.
+
+    param:
+        path: Caminho do arquivo de código.
+    return:
+        Código formatado e separado por linhas com destaque 
+        para palavras reservadas e operadores.
+        
+    """
+    res = None
+    with open(path, "r", encoding="utf-8") as file:
+        codigo = file.read()
+    res = codigo.replace(" ", "")
+
+    # destaca as palavras reservadas
+    for symbol in ["def", "if", "else", "elif", "for", "while", "return"]:
+        res = res.replace(symbol, f"{symbol} ")
+
+    # destaca os operadores
+    for symbol in ["=", "<=", ">=", "==", "and", "or", "not", "is"]:
+        res = res.replace(symbol, f" {symbol} ")
+
+    # Separa as linhas
+    lines = []
+    for line in res.split("\n"):
+        lines.append(line)
+    return lines
